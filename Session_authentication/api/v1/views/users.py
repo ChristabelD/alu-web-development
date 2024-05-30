2a6c149016cad6c8f4868be23fd64992e7efe496
Christabel Adams <C.adams@alustudent.com>
2024-05-30 17:37:29 +0200

    5

--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -27,17 +27,21 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
-    if user_id == "me":
-        if request.current_user is None:
-            abort(404)
-        user = request.current_user
-        return jsonify(user.to_json())
+    if user_id == 'me' and request.current_user is None:
+        abort(404)
     user = User.get(user_id)
     if user is None:
         abort(404)
-    if request.current_user is None:
-        abort(404)
     return jsonify(user.to_json())
+
+
+@app_views.route('/users/me', methods=['GET'], strict_slashes=False)
+def retrieveMe() -> str:
+    """ Retrieve the authenticated User object.
+    Return:
+        User object JSON represented
+    """
+    return jsonify(request.current_user.to_json())
 
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
